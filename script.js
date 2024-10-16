c0f4bfbd08356b567642f2a5da42c20bd928fc5d
DAB804 <157375910+DAB804@users.noreply.github.com>
2024-10-16 21:52:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const motapplication="BONJOUR"
+let motutilisateur=prompt( "Entrée le mot:"+motapplication)
+if(motutilisateur===motapplication){
+    console.log("BRAVO!")
+}else{
+    console.log("ERREUR")
+}
+
+
